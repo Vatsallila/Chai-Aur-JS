49a5af90d6d0715aa77912798def04892bcfc0ee
Vatsallila <vatsall2003@gmail.com>
2024-02-20 19:08:50 +0530

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// # Primitive
+
+// 7 types : String, Number, boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isloggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+//const bigNumber = 134412342134123443434334n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj"]
+let myObj = {
+    name: "Vatsal",
+    age: "20",
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof anotherId);
+
+
